127766426c1310f1ca247e0c5ff4bd4aa95267e6
JeryPSK <ap-pskov@ya.ru>
2026-03-11 00:00:42 +0300

    fix: Поправить багу с роутингом на индекс админки

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,6 @@ Route::get('/', [PageController::class, 'index'])->name('home');
 // Карта сайта
 Route::get('/sitemap.xml', [SitemapController::class, 'index'])->name('sitemap');
 
-Route::get('/{page}', [PageController::class, 'index'])->name('page.show');
-
+Route::get('/{slug}', [PageController::class, 'index'])
+    ->where('slug', '^(?!admin|login|register|password|api|sanctum|_ignition).*$')
+    ->name('page.show');
